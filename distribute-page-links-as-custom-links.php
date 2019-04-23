f82760d9784f2700df68e9fc7ade68b7dd8fedfd
ulkish <dev.hugomoran@gmail.com>
2019-04-23 15:40:21 -0300

    Trying to put everything inside a class

--- distribute-page-links-as-custom-links.php
+++ distribute-page-links-as-custom-links.php
@@ -1,13 +1,14 @@
 <?php
-/**
- * Distribute ACF Page Links as Custom Links
- *
- * Plugin Name: Distribute ACF Page Links as Custom Links
- * Plugin URI:
- * Description: This is an addon for Distributor plugin.
- * Version:     1.0
- * Author:      Tipit.net
- */
+/*
+Plugin Name: Distribute Page Links as Custom Links
+Plugin URI:
+Description: Allows you to distribute page links as custom links using the Distributor plugin.
+Version: 1.0.0
+Author: hugomoran
+Author URI: https://convistaalmar.com.ar/
+License: GPL2+
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+*/
 
 if ( ! defined( 'ABSPATH' ) ) {
 	die( 'Invalid request.' );
@@ -19,9 +20,12 @@ if ( ! defined( 'ABSPATH' ) ) {
 // Create a custom link inside the new post, place the 'guid' inside.
 // Erase page links.
 
+add_action( 'dt_push_post', 'push_acf_page_link', 10, 4 );
+add_action( 'dt_pull_post', 'pull_acf_page_link', 10, 3 );
 
 function push_acf_page_link( $new_post_id, $original_post_id, $args, $site ) {
 
+    echo 'Push_acf_page_link function.';
     $destination_blog_id = (is_numeric($site)) ? $site : $site->site->blog_id;
 
     // Switch to original blog to get id
@@ -61,7 +65,7 @@ function push_acf_page_link( $new_post_id, $original_post_id, $args, $site ) {
 
     return false;
 }
-add_action( 'dt_push_post', 'push_acf_page_link', 11, 4 );
+
 
 
 function pull_acf_page_link( $new_post_id, $args, $post_array ) {
@@ -70,4 +74,3 @@ function pull_acf_page_link( $new_post_id, $args, $post_array ) {
     push_acf_page_link( $new_post_id, $original_post_id, $args, $destination_blog_id );
 
 }
-add_action( 'dt_pull_post', 'pull_acf_page_link', 10, 3 );
